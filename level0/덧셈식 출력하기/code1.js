5664ca18a149982626ad0183c1a0c84b17ecacdc
LeeSH <snghyun331@gmail.com>
2025-02-26 23:55:53 +0900

    [level0] 덧셈식 출력하기

---
+++ level0/덧셈식 출력하기/code1.js
@@ -0,0 +1,13 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let a;
+let b;
+rl.on("line", function (line) {
+  [a, b] = line.split(" ").map(Number);
+}).on("close", function () {
+  console.log(`${a} + ${b} = ${a + b}`);
+});
